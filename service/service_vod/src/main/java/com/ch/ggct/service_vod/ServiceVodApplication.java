5badd45688804edeeb952a4130815046f21bae88
chenghao <ch.163@com>
2022-09-16 13:13:54 +0800

    整合Swagger接口测试

--- service/service_vod/src/main/java/com/ch/ggct/service_vod/ServiceVodApplication.java
+++ service/service_vod/src/main/java/com/ch/ggct/service_vod/ServiceVodApplication.java
@@ -2,6 +2,7 @@ package com.ch.ggct.service_vod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author chenghao
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @备注：
  * @data 2022年09月11日 22:48
  */
+@EnableSwagger2
 @SpringBootApplication
 public class ServiceVodApplication {
     public static void main(String[] args) {
--- service/service_vod/src/main/java/com/ch/ggct/service_vod/controller/TeacherController.java
+++ service/service_vod/src/main/java/com/ch/ggct/service_vod/controller/TeacherController.java
@@ -1,15 +1,14 @@
 package com.ch.ggct.service_vod.controller;
 
-
-
 import com.ch.ggct.model.vod.Teacher;
-import com.ch.ggct.service_vod.mapper.TeacherMapper;
 import com.ch.ggct.service_vod.service.TeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.ibatis.annotations.Param;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,16 +20,30 @@ import java.util.List;
  * @author ch
  * @since 2022-09-11
  */
+@Api(tags = "讲师管理系统")
 @RestController
 @RequestMapping("/admin/vod/teacher")
 public class TeacherController {
     @Autowired
     private TeacherService teacherService;
-
+    /*
+        查询所有讲师
+     */
+    @ApiOperation("返回所有讲师接口")
     @GetMapping("findAll")
     public List<Teacher> findAll(){
         List<Teacher> list = teacherService.list();
         return list;
+    }
+    /*
+        根据ID删除讲师
+     */
+    @ApiOperation("逻辑删除讲师")
+    //TODO
+    @DeleteMapping("remove/{id}")
+    public boolean removeTeacher(@ApiParam(name = "id",value = "ID") @PathVariable("id") long id){
+        boolean b = teacherService.removeById(id);
+        return b;
     }
 
 
