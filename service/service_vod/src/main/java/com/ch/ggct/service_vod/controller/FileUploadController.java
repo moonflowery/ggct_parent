72619a27960767bf4e0aa273c68263504c69e38b
chenghao <ch.163@com>
2022-09-22 11:05:29 +0800

    2.发布课程 3.修改课程

--- service/service_vod/src/main/java/com/ch/ggct/service_vod/controller/FileUploadController.java
+++ service/service_vod/src/main/java/com/ch/ggct/service_vod/controller/FileUploadController.java
@@ -6,14 +6,12 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Api(tags = "文件上传接口")
 @RestController
+@CrossOrigin
 @RequestMapping("/admin/vod/file")
 public class FileUploadController {
     @Autowired
@@ -22,7 +20,8 @@ public class FileUploadController {
      * 文件上传
      */
     @ApiOperation(value = "文件上传")
-    @PostMapping("upload")
+
+    @PostMapping(value = "upload",consumes = "multipart/form-data")
     public Result upload(
             @ApiParam(name = "file", value = "文件", required = true)
             @RequestParam("file") MultipartFile file) {
